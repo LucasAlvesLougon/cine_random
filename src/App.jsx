805064314d6765eef1d2e6018a825cabc0dd69a3
Lucas <lucas.lougon@globalsys.com.br>
2026-08-31 16:08:08 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+    function App() {
+      return (
+        <div>
+          <h1>Sorteador de Filmes</h1>
+          <p>Nosso React está rodando!</p>
+        </div>
+      )
+    }
+
+    export default App
